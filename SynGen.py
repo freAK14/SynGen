83ffde3eecac0c4e136392b57b9bcb6d5a9b332b
Akash Kothare <akashsocial14@gmail.com>
2021-01-26 05:19:57 +0530

    Update SynGen.py

--- SynGen.py
+++ SynGen.py
@@ -1,5 +1,6 @@
 #importing required libraries
 import os
+import random
 from random import randint, choice
 import pandas as pd
 
@@ -16,9 +17,32 @@ class syngen:
         self.seed = seed
         self.randnum = randint(1, 9)
         
-        #initialize license-plate state list
-        #initialize domain list
+        #initializing license-plate state list
+        self.state_initials = self._initialize_state_initials()
+        #initializing email domain list
+        self.domain_list =  self._initialize_email_domains()
         
+        
+    def _initialize_state_initials(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = dir_path + os.sep + "state_initials.txt"
+        
+        state_initials = []
+        with open(path) as f:
+            state_initials = [str(line).strip() for line in f.readlines()]
+            
+        return state_initials
+    
+    def _initialize_email_domains(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = dir_path + os.sep + "email_domains.txt"
+        
+        email_domains = []
+        with open(path) as f:
+            email_domains = [str(line).strip() for line in f.readlines()]
+            
+        return email_domains
+    
     def phone_num(self, seed = None):
         """
         Generates 10 digit Indian phone number in xxxxx-xxxxx format
@@ -33,12 +57,41 @@ class syngen:
         
         return phone_format.format(p1 = p1, p2 = p2)
     
-    def license_plate():
-        #need .txt file
+    def license_plate(self, seed = None):
+        """
+        Generates vehicle license plate in the format MH-31-VS-9999
+        seed: Currently not used. Uses seed from the pydb class if chosen by user
+        """
+        random.seed(self.seed)
         
-    def college_regno():
-        #format 
+        license_plate_format = "{p1}{p2}{p3}{p4}{p5}{p6}{p7}"    #try without placeholders for '-'
+        state_in = choice(self.state_initials)
         
+        p1 = str(state_in)
+        p2 = "-"
+        p3 = "".join([str(randint(0, 9)) for i in range(2)])
+        p4 = "-"
+        p5 = "".join([chr(randint(65, 90)) for _ in range(2)])
+        p6 = "-"
+        p7 = "".join([str(randint(0, 9)) for _ in range(4)])
         
+        return license_plate_format.format(p1 = p1, p2 = p2, p3 = p3, p4 = p4, p5 = p5, p6 = p6, p7 = p7)
         
+    
+    def college_regno(self, seed = None):
+        """
+        Generates a 15-character college registration number in the format 2018ACSC1101085
+        seed: Currently not used. Uses seed from the pydb class if chosen by user
+        """
+        random.seed(self.seed)
+        
+        reg_no_format = "{p1}{p2}{p3}"
+        dept_list = ["ACSC", "ACIV", "AIFT", "AETX"]
+        dept = choice(dept_list)
+        
+        p1 = str(randint(2010, 2022))
+        p2 = "".join([str(dept)])
+        p3 = "".join([str(randint(0, 9)) for _ in range (7)]) 
+        
+        return reg_no_format.format(p1 =p1, p2 = p2, p3 = p3)
         
